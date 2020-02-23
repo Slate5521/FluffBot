413ccbd5dfabb6b0c5351b1f4b85d7a76516cdfc
Slate5521 <30555890+Slate5521@users.noreply.github.com>
2020-02-23 10:35:34 -0800

    /shrug

--- FluffyEars/SaveUtility/Program.cs
+++ FluffyEars/SaveUtility/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
-using FluffyEars;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace SaveUtility
 {
@@ -13,11 +14,44 @@ namespace SaveUtility
                 string file = args[0];
                 if(File.Exists(file))
                 {
-                    string md5 = SaveFile.GetFileMD5(file);
+                    string md5 = GetFileMD5(file);
 
                     using(StreamWriter sw = new StreamWriter(file + @".MD5", false))
                         sw.Write(md5);
                 }
+            }
+
+            static string GetFileMD5(string file)
+            {
+                string fileContents = ReadFile(file);
+
+                return GetHash(fileContents);
+            }
+
+            static string ReadFile(string file)
+            {
+                string returnVal;
+
+                using (FileStream fs = File.OpenRead(file))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        returnVal = sr.ReadToEnd();
+                    }
+                }
+                return returnVal;
+            }
+
+            static string GetHash(string contents)
+            {
+                byte[] hash;
+
+                using (MD5 fileMD5 = MD5.Create())
+                {
+                    hash = fileMD5.ComputeHash(Encoding.UTF8.GetBytes(contents));
+                }
+
+                return Convert.ToBase64String(hash);
             }
         }
     }
