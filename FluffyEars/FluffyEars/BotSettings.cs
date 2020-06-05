904dd49aa4abe2139777929b46a5ba273d8a73dc
Slate5521 <30555890+Slate5521@users.noreply.github.com>
2020-06-05 03:56:18 -0700

    Prettify BotSettings.cs

--- FluffyEars/FluffyEars/BotSettings.cs
+++ FluffyEars/FluffyEars/BotSettings.cs
@@ -3,70 +3,15 @@
 
 using System;
 using System.Collections.Generic;
-using System.Text;
-using System.Linq;
-using DSharpPlus;
 using DSharpPlus.Entities;
 using System.Threading.Tasks;
-using System.IO;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using Newtonsoft.Json;
-using FluffyEars.Reminders;
 
 namespace FluffyEars
 {
     /// <summary>This is the static class that houses all the bot's settings. </summary>
     public static class BotSettings
     {
-        private static SaveFile saveFile = new SaveFile(BaseFile);
-        /// <summary>A JSON serializable struct that contains the actual settings.</summary>
-        private struct botSettings_
-        {
-            /// <summary>Audit log channel.</summary>
-            public ulong FilterChannelId;
-            /// <summary>A list of channels that will not be checked.</summary>
-            public List<ulong> ExcludedChannels;
-            /// <summary>Whether or not the bot should announce it has started.</summary>
-            public bool StartMessageEnabled;
-
-            public List<string> FrozenNames;
-        }
-
-        /// <summary>Default bot settings.</summary>
-        static botSettings_ DefaultBotSettings = new botSettings_
-        {
-            FilterChannelId = 674884683166646283,
-            ExcludedChannels = new List<ulong>(),
-            StartMessageEnabled = false,
-            FrozenNames = new List<string>() 
-            { 
-                "frozen",
-                "frozo",
-                "forza",
-                "freezy"
-            }
-        };
-
-        /// <summary>Bot settings!</summary>
-        private static botSettings_ botSettings;
-        
-        public static ulong FilterChannelId
-        {
-            get => botSettings.FilterChannelId;
-            set => botSettings.FilterChannelId = value;
-        }
-
-        public static bool StartMessageEnabled
-        {
-            get => botSettings.StartMessageEnabled;
-            set => botSettings.StartMessageEnabled = value;
-        }
-
-        // I will not be documenting these for the moment.
-        #region Initialization, deconstruction commands
-
-        public const string BaseFile = "settings";
+        private const string BaseFile = "settings";
         private static readonly object lockObj = (object)
     @"
          ,
@@ -90,35 +35,99 @@ namespace FluffyEars
 (' ~----( ~   Y.  )";
 ///////////////////////
 
+        private static SaveFile saveFile = new SaveFile(BaseFile);
+        /// <summary>A JSON serializable struct that contains the actual settings.</summary>
+        private struct botSettings_
+        {
+            /// <summary>Audit log channel.</summary>
+            public ulong FilterChannelId;
+            /// <summary>A list of channels that will not be checked.</summary>
+            public List<ulong> ExcludedChannels;
+            /// <summary>Whether or not the bot should announce it has started.</summary>
+            public bool StartMessageEnabled;
+
+            public List<string> FrozenNames;
+        }
+
+        /// <summary>Default bot settings.</summary>
+        private static botSettings_ DefaultBotSettings = new botSettings_
+        {
+            FilterChannelId = 674884683166646283,
+            ExcludedChannels = new List<ulong>(),
+            StartMessageEnabled = false,
+            FrozenNames = new List<string>() 
+            { 
+                "frozen",
+                "frozo",
+                "forza",
+                "freezy"
+            }
+        };
+
+        /// <summary>Bot settings!</summary>
+        private static botSettings_ botSettings;
+
+        #region Public Fields
+
+        public static ulong FilterChannelId
+        {
+            get => botSettings.FilterChannelId;
+            set => botSettings.FilterChannelId = value;
+        }
+
+        public static bool StartMessageEnabled
+        {
+            get => botSettings.StartMessageEnabled;
+            set => botSettings.StartMessageEnabled = value;
+        }
+
+        #endregion Public Fields
+        // ################################
+        #region Save/Load Methods
+
         public static void Default()
         {
             botSettings = DefaultBotSettings;
+
             Save();
         }
 
-        public static void Save() => saveFile.Save<botSettings_>(botSettings, lockObj);
+        public static void Save() 
+            => saveFile.Save(botSettings, lockObj);
 
-        public static bool CanLoad() => saveFile.IsExistingSaveFile();
+        public static bool CanLoad() 
+            => saveFile.IsExistingSaveFile();
 
         public static void Load()
         {
             if (CanLoad())
+            {
                 botSettings = saveFile.Load<botSettings_>(lockObj);
-            else Default();
+            }
+            else
+            {
+                Default();
+            }
         }
 
-        #endregion Initialization, deconstruction commands
+        #endregion Save/Load Methods
+        // ################################
+        #region Frozen Methods
 
         public static void AddFrozenName(string name)
         {
             if (!IsFrozenName(name))
+            {
                 botSettings.FrozenNames.Add(name);
+            }
         }
 
         public static void RemoveFrozenName(string name)
         {
             if (IsFrozenName(name))
+            {
                 botSettings.FrozenNames.Remove(name);
+            }
         }
 
         public static bool IsFrozenName(string name)
@@ -127,23 +136,30 @@ namespace FluffyEars
         public static string[] GetFrozenNames()
             => botSettings.FrozenNames.ToArray();
 
-        #region Channel exclusion
+        #endregion Frozen Methods
+        // ################################
+        #region Channel Exclusion
 
         /// <summary>Exclude the channel from bad word detection.</summary>
         public static void ExcludeChannel(DiscordChannel chan)
         {
-            if(!botSettings.ExcludedChannels.Contains(chan.Id))
+            if (!botSettings.ExcludedChannels.Contains(chan.Id))
+            {
                 botSettings.ExcludedChannels.Add(chan.Id);
+            }
         }
 
         /// <summary>Un-exclude the channel from bad word detection.</summary>
         public static void IncludeChannel(DiscordChannel chan)
         {
             if (botSettings.ExcludedChannels.Contains(chan.Id))
+            {
                 botSettings.ExcludedChannels.Remove(chan.Id);
+            }
         }
 
-        public static int GetExcludedChannelsCount => botSettings.ExcludedChannels.Count;
+        public static int GetExcludedChannelsCount 
+            => botSettings.ExcludedChannels.Count;
 
         /// <summary>Get excluded channels.</summary>
         public static async Task<DiscordChannel[]> GetExcludedChannels()
@@ -154,17 +170,22 @@ namespace FluffyEars
             {
                 channels = new DiscordChannel[botSettings.ExcludedChannels.Count];
 
-                for(int i = 0; i < botSettings.ExcludedChannels.Count; i++)
+                for (int i = 0; i < botSettings.ExcludedChannels.Count; i++)
+                {
                     channels[i] = await Bot.BotClient.GetChannelAsync(botSettings.ExcludedChannels[i]);
+                }
             }
             else
+            {
                 channels = Array.Empty<DiscordChannel>();
+            }
 
             return channels;
         }
         /// <summary>Check if the channel is excluded.</summary>
         /// <returns>True if the channel is excluded.</returns>
-        public static bool IsChannelExcluded(DiscordChannel chan) => botSettings.ExcludedChannels.Contains(chan.Id);
+        public static bool IsChannelExcluded(DiscordChannel chan) 
+            => botSettings.ExcludedChannels.Contains(chan.Id);
 
         #endregion Channel exclusion
     }
