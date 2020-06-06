6e252e02de237b5c9df7cb17bbc13a62ef8618f3
Slate5521 <30555890+Slate5521@users.noreply.github.com>
2020-06-05 22:30:16 -0700

    Update FilterCommands.cs

--- FluffyEars/FluffyEars/Commands/FilterCommands.cs
+++ FluffyEars/FluffyEars/Commands/FilterCommands.cs
@@ -21,151 +21,247 @@ namespace FluffyEars.Commands
         public async Task FilterAddMask(CommandContext ctx, params string[] args)
         {
             // Check if the user can use commands.
-            if (ctx.Member.GetHighestRole().IsSeniorModOrHigher() && args.Length > 0)
+            if (!ctx.Member.GetHighestRole().IsSeniorModOrHigher())
             {
-                await ctx.TriggerTypingAsync();
-
-                string mask = ParamsToString(args);
-
-                // DEB!
-                DiscordEmbedBuilder deb = new DiscordEmbedBuilder();
-
-                bool success = !FilterSystem.IsMask(mask) && !Excludes.IsExcluded(mask);
-
-                if (success)
-                {
-                    FilterSystem.AddMask(mask);
-                    FilterSystem.Save();
-
-                    deb.WithColor(DiscordColor.Green);
-                    deb.WithDescription(ChatObjects.GetSuccessMessage(@"I was able to add the mask you gave me!"));
-                }
-                else
-                {
-                    deb.WithColor(DiscordColor.Red);
-                    deb.WithDescription(ChatObjects.GetErrMessage(@"I was unable able to add the mask you gave me... It already exists..."));
-                }
-
-                deb.WithThumbnail(ChatObjects.URL_FILTER_ADD);
-                deb.AddField(@"Mask", $"`{mask}`");
-
-                await ctx.Channel.SendMessageAsync(embed: deb.Build());
+                await Bot.NotifyInvalidPermissions
+                       (
+                           requiredRole: Role.SeniorModerator,
+                           command: ctx.Command.Name,
+                           channel: ctx.Channel,
+                           caller: ctx.Member
+                       );
             }
-        }
+            else
+            {
+                if (args.Length > 0)
+                {
+                    await ctx.TriggerTypingAsync();
+
+                    DiscordEmbedBuilder deb;
+
+                    string mask = ParamsToString(args);
+
+                    if (!FilterSystem.IsMask(mask) && !Excludes.IsExcluded(mask))
+                    {
+                        FilterSystem.AddMask(mask);
+                        FilterSystem.Save();
+
+                        deb = new DiscordEmbedBuilder(ChatObjects.FormatEmbedResponse
+                        (
+                            title: @"Add Mask",
+                            description: ChatObjects.GetNeutralMessage(@"I was able to add the mask you gave me!"),
+                            color: ChatObjects.SuccessColor,
+                            thumbnail: ChatObjects.URL_FILTER_ADD
+                        ));
+                    }
+                    else
+                    {
+                        deb = new DiscordEmbedBuilder(ChatObjects.FormatEmbedResponse
+                        (
+                            title: @"Unable to Add Mask",
+                            description: ChatObjects.GetNeutralMessage(@"I was unable to add the mask you gave me. It's already a mask or a filter exclude..."),
+                            color: ChatObjects.ErrColor,
+                            thumbnail: ChatObjects.URL_FILTER_ADD
+                        ));
+                    }
+
+                    deb.AddField(@"Mask", $"`{mask}`");
+
+                    await ctx.Channel.SendMessageAsync(embed: deb.Build());
+                } // end if
+            } // end else
+        } // end method
 
         /// <summary>Remove a single bad word from the bad word list, if it exists.</summary>
         [Command("-filter")]
         public async Task RemoveFilterMask(CommandContext ctx, params string[] args)
         {
             // Check if the user can use commands.
-            if (ctx.Member.GetHighestRole().IsSeniorModOrHigher() && args.Length > 0)
+            if (!ctx.Member.GetHighestRole().IsSeniorModOrHigher())
             {
-                await ctx.TriggerTypingAsync();
-
-                string mask = ParamsToString(args);
-
-                // DEB!
-                DiscordEmbedBuilder deb = new DiscordEmbedBuilder();
-
-                if (FilterSystem.IsMask(mask))
-                {
-                    FilterSystem.RemoveMask(mask);
-                    FilterSystem.Save();
-
-                    deb.WithColor(DiscordColor.Green);
-                    deb.WithDescription(ChatObjects.GetSuccessMessage(@"I was able to remove the mask you gave me!"));
-                }
-                else
-                {
-                    deb.WithColor(DiscordColor.Red);
-                    deb.WithDescription(ChatObjects.GetErrMessage(@"I was unable able to remove the mask you gave me... It doesn't exist..."));
-                }
-
-                deb.WithThumbnail(ChatObjects.URL_FILTER_ADD);
-                deb.AddField(@"Mask", $"`{mask}`");
-
-                await ctx.Channel.SendMessageAsync(embed: deb.Build());
+                await Bot.NotifyInvalidPermissions
+                       (
+                           requiredRole: Role.SeniorModerator,
+                           command: ctx.Command.Name,
+                           channel: ctx.Channel,
+                           caller: ctx.Member
+                       );
             }
-        }
+            else
+            {
+                if (args.Length > 0)
+                {
+                    await ctx.TriggerTypingAsync();
+
+                    string mask = ParamsToString(args);
+
+                    DiscordEmbedBuilder deb;
+
+                    if (FilterSystem.IsMask(mask))
+                    {
+                        FilterSystem.RemoveMask(mask);
+                        FilterSystem.Save();
+
+                        deb = new DiscordEmbedBuilder(ChatObjects.FormatEmbedResponse
+                        (
+                            title: @"Add Mask",
+                            description: ChatObjects.GetNeutralMessage(@"I was able to remove the mask you gave me!"),
+                            color: ChatObjects.SuccessColor,
+                            thumbnail: ChatObjects.URL_FILTER_SUB
+                        ));
+                    }
+                    else
+                    {
+                        deb = new DiscordEmbedBuilder(ChatObjects.FormatEmbedResponse
+                        (
+                            title: @"Unable to Add Mask",
+                            description: ChatObjects.GetNeutralMessage(@"I was unable to add the mask you gave me. It doesn't exist..."),
+                            color: ChatObjects.ErrColor,
+                            thumbnail: ChatObjects.URL_FILTER_SUB
+                        ));
+                    }
+
+                    deb.AddField(@"Mask", $"`{mask}`");
+
+                    await ctx.Channel.SendMessageAsync(embed: deb.Build());
+                } // end if
+            } // end else
+        } // end method
 
         /// <summary>List all the bad words currently in the bad word list</summary>
         [Command("filterlist")]
         public async Task ListFilterMasks(CommandContext ctx)
         {
-            if (ctx.Member.GetHighestRole().IsModOrHigher())
+            if (!ctx.Member.GetHighestRole().IsModOrHigher())
+            {
+                await Bot.NotifyInvalidPermissions
+                       (
+                           requiredRole: Role.Moderator,
+                           command: ctx.Command.Name,
+                           channel: ctx.Channel,
+                           caller: ctx.Member
+                       );
+            }
+            else
             {
                 StringBuilder sb = new StringBuilder();
 
                 foreach (string mask in FilterSystem.GetMasks())
-                    sb.AppendLine(mask);
+                    sb.AppendLine($"`{mask}`");
 
-                DiscordEmbedBuilder deb = new DiscordEmbedBuilder()
-                {
-                    Color = DiscordColor.LightGray,
-                    Description = sb.ToString(),
-                    Title = "FILTER MASK LIST",
-                };
+                await ctx.Channel.SendMessageAsync(
+                    embed:
 
-                deb.WithThumbnail(ChatObjects.URL_FILTER_GENERIC);
-
-                await ctx.Channel.SendMessageAsync(embed: deb.Build());
+                    ChatObjects.FormatEmbedResponse
+                    (
+                        title: "List of Masks",
+                        description: sb.ToString(),
+                        color: ChatObjects.NeutralColor,
+                        thumbnail: ChatObjects.URL_FILTER_GENERIC
+                    ));
             }
         }
 
         [Command("+filterexclude")]
         public async Task FilterExclude(CommandContext ctx, params string[] args)
         {
-            if (ctx.Member.GetHighestRole().IsSeniorModOrHigher() && args.Length > 0)
+            if (!ctx.Member.GetHighestRole().IsSeniorModOrHigher())
+            {
+                await Bot.NotifyInvalidPermissions
+                       (
+                           requiredRole: Role.SeniorModerator,
+                           command: ctx.Command.Name,
+                           channel: ctx.Channel,
+                           caller: ctx.Member
+                       );
+            }
+            else
             {
                 await ctx.TriggerTypingAsync();
 
                 string phrase = ParamsToString(args);
+                DiscordEmbedBuilder deb;
 
                 // Cancels:
-                if (FilterSystem.IsMask(phrase))
+                if (FilterSystem.IsMask(phrase) || Excludes.IsExcluded(phrase))
                 {
-                    await ctx.Channel.SendMessageAsync(
-                        ChatObjects.GetErrMessage(@"Cannot add that phrase. It's a filter word..."));
-                    return;
+                    deb = new DiscordEmbedBuilder(ChatObjects.FormatEmbedResponse
+                    (
+                        title: @"Unable to Add Exclude",
+                        description: ChatObjects.GetErrMessage(@"I was unable to exclude that phrase. It's either excluded already or a mask..."),
+                        color: ChatObjects.ErrColor,
+                        thumbnail: ChatObjects.URL_FILTER_ADD
+                    ));
                 }
-                if (Excludes.IsExcluded(phrase))
+                else
                 {
-                    await ctx.Channel.SendMessageAsync(
-                        ChatObjects.GetErrMessage(@"Cannot add that phrase. It's already excluded..."));
-                    return;
+                    Excludes.AddPhrase(phrase);
+                    Excludes.Save();
+
+                    deb = new DiscordEmbedBuilder(ChatObjects.FormatEmbedResponse
+                    (
+                        title: @"Add Exclude",
+                        description: ChatObjects.GetSuccessMessage(@"I was able to exclude that phrase!"),
+                        color: ChatObjects.SuccessColor,
+                        thumbnail: ChatObjects.URL_FILTER_ADD
+                    ));
                 }
 
-                Excludes.AddPhrase(phrase);
-                Excludes.Save();
+                deb.AddField(@"Phrase", phrase);
 
-                await ctx.Channel.SendMessageAsync(
-                    ChatObjects.GetSuccessMessage(
-                        String.Format("I excluded the phrase {0}!", phrase)));
-
+                await ctx.Channel.SendMessageAsync(embed: deb.Build());
             }
         }
 
         [Command("-filterexclude")]
         public async Task FilterRemoveExclude(CommandContext ctx, params string[] args)
         {
-            if (ctx.Member.GetHighestRole().IsSeniorModOrHigher() && args.Length > 0)
+            if (!ctx.Member.GetHighestRole().IsSeniorModOrHigher())
             {
-                await ctx.TriggerTypingAsync();
-
-                string phrase = ParamsToString(args);
-
-                if (!Excludes.IsExcluded(phrase))
+                await Bot.NotifyInvalidPermissions
+                       (
+                           requiredRole: Role.SeniorModerator,
+                           command: ctx.Command.Name,
+                           channel: ctx.Channel,
+                           caller: ctx.Member
+                       );
+            }
+            else 
+            {
+                if (args.Length > 0)
                 {
-                    await ctx.Channel.SendMessageAsync(
-                        ChatObjects.GetErrMessage(@"Cannot un-exclude that phrase. It's not already excluded..."));
-                }
-                else
-                {
-                    Excludes.RemovePhrase(phrase);
-                    Excludes.Save();
+                    await ctx.TriggerTypingAsync();
 
-                    await ctx.Channel.SendMessageAsync(
-                        ChatObjects.GetSuccessMessage(@"I removed that phrase from exclusion!"));
+                    string phrase = ParamsToString(args);
+                    DiscordEmbedBuilder deb;
+
+                    if (!Excludes.IsExcluded(phrase))
+                    {
+                        deb = new DiscordEmbedBuilder(ChatObjects.FormatEmbedResponse
+                        (
+                            title: @"Unable to Remove Exclude",
+                            description: ChatObjects.GetErrMessage(@"I was unable to remove that phrase from exclusion. It's not excluded already..."),
+                            color: ChatObjects.ErrColor,
+                            thumbnail: ChatObjects.URL_FILTER_SUB
+                        ));
+                    }
+                    else
+                    {
+                        Excludes.RemovePhrase(phrase);
+                        Excludes.Save();
+
+                        deb = new DiscordEmbedBuilder(ChatObjects.FormatEmbedResponse
+                        (
+                            title: @"Removed Exclude",
+                            description: ChatObjects.GetSuccessMessage(@"I removed that phrase from exclusion!"),
+                            color: ChatObjects.SuccessColor,
+                            thumbnail: ChatObjects.URL_FILTER_SUB
+                        ));
+                    }
+
+                    deb.AddField(@"Phrase", phrase);
+
+                    await ctx.Channel.SendMessageAsync(embed: deb.Build());
                 }
             }
         }
@@ -173,29 +269,33 @@ namespace FluffyEars.Commands
         [Command("filterexcludes")]
         public async Task ListFilterExcludes(CommandContext ctx)
         {
-            if (ctx.Member.GetHighestRole().IsModOrHigher())
+            if (!ctx.Member.GetHighestRole().IsModOrHigher())
             {
-                StringBuilder sb = new StringBuilder();
-
-                // Cancels:
-                if (Excludes.GetPhraseCount() == 0)
-                {
-                    await ctx.Channel.SendMessageAsync(
-                        ChatObjects.GetNeutralMessage(@"There are no filter excludes..."));
-                    return;
-                }
+                await Bot.NotifyInvalidPermissions
+                       (
+                           requiredRole: Role.Moderator,
+                           command: ctx.Command.Name,
+                           channel: ctx.Channel,
+                           caller: ctx.Member
+                       );
+            }
+            else
+            { 
+                var stringBuilder = new StringBuilder();
 
                 foreach (string word in Excludes.GetPhrases())
-                    sb.AppendLine(word);
+                    stringBuilder.AppendLine($"`{word}`");
 
-                DiscordEmbedBuilder deb = new DiscordEmbedBuilder()
-                {
-                    Color = DiscordColor.LightGray,
-                    Description = sb.ToString(),
-                    Title = "FILTER LIST EXCLUDES"
-                };
+                await ctx.Channel.SendMessageAsync(
+                    embed:
 
-                await ctx.Channel.SendMessageAsync(embed: deb.Build());
+                    ChatObjects.FormatEmbedResponse
+                    (
+                        title: "List of Excludes",
+                        description: stringBuilder.ToString(),
+                        color: ChatObjects.NeutralColor,
+                        thumbnail: ChatObjects.URL_FILTER_GENERIC
+                    ));
             }
         }
 
@@ -203,11 +303,11 @@ namespace FluffyEars.Commands
 
         private static string ParamsToString(string[] paramsStr)
         {
-            StringBuilder sb = new StringBuilder();
+            var stringBuilder = new StringBuilder();
 
-            sb.AppendJoin(' ', paramsStr);
+            stringBuilder.AppendJoin(' ', paramsStr);
 
-            return sb.ToString();
+            return stringBuilder.ToString();
         }
 
     }
