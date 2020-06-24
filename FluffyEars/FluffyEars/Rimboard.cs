c447bb9c0290cc4c67f44627eb0445c40570f48b
Slate5521 <30555890+Slate5521@users.noreply.github.com>
2020-06-24 03:59:52 -0700

    Remove fucky pinning check

--- FluffyEars/FluffyEars/Rimboard.cs
+++ FluffyEars/FluffyEars/Rimboard.cs
@@ -45,30 +45,6 @@ namespace FluffyEars
                     if (e.Channel.Id.Equals(BotSettings.RimboardChannelId))
                     {   // If this is in our rimboard channel, let's pin it.
 
-                        // Let's check if it's an informational embed first.
-                        if(message_no_cache.Content.Length == 0)
-                        {   // It is an informational embed, most likely.
-                            
-                            var infoEmbed = message_no_cache.Embeds[0];
-
-                            if(infoEmbed.Footer.Text.Length > 0)
-                            {   // Let's check its footer for the real message.
-
-                                if(ulong.TryParse(infoEmbed.Footer.Text, out ulong snowflake))
-                                {
-                                    DiscordMessage backup = message_no_cache;
-
-                                    try
-                                    {
-                                        message_no_cache = await message_no_cache.Channel.GetMessageAsync(snowflake);
-                                    } catch
-                                    {
-                                        message_no_cache = backup;
-                                    }
-                                }
-                            }
-                        }
-
                         var pinnedMessages = await rimboardChannel.GetPinnedMessagesAsync();
 
                         if (pinnedMessages.Count == 50)
