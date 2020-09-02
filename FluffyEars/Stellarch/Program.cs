371805366aec0a75f6bfd9d23a7a3c2682d7e77f
Slate5521 <jekksie@gmail.com>
2020-09-01 23:43:13 -0700

    a lot

--- FluffyEars/Stellarch/Program.cs
+++ FluffyEars/Stellarch/Program.cs
@@ -10,21 +10,138 @@
 
 using System;
 using System.IO;
+using BigSister.Settings;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.IO.Pipes;
 
 namespace BigSister
 {
     public static class Program
     {
+        protected static class Files
+        {
+            public static string ExecutableDirectory
+            {
+                get => AppDomain.CurrentDomain.BaseDirectory;
+            }
+
+            const string IDENTITY_FILE = @"sav\identity.json";
+            public static string IdentityFile
+            {
+                get => Path.Combine(ExecutableDirectory, IDENTITY_FILE);
+            }
+
+            const string SETTINGS_FILE = @"sav\settings.json";
+            public static string SettingsFile
+            {
+                get => Path.Combine(ExecutableDirectory, SETTINGS_FILE);
+            }
+        }
+        
+        static SaveFile BotSettingsFile;
+        static Identity Identity;
+
+        static object botSettingsLock = new object();
+
         static void Main(string[] args)
         {
-            // Process CLI
-            if(ProcessCLI(args))
+            // ----------------
+            // TODO: Process CLI
+            //if(ProcessCLI(args))
+            //{
+            //    Environment.Exit(0);
+            //}
+
+            BotSettings botSettings;
+            bool loadSuccess;
+
+
+            // ----------------
+            // Load authkey and webhooks.
+            Console.Write("Loading identity... ");
+            Identity = LoadIdentity();
+            Console.WriteLine("Found authkey and {0} webhook{1}.", 
+                Identity.Webhooks.Count,
+                Identity.Webhooks.Count == 1 ? '\0' : 's');
+
+            // ----------------
+            // Load bot settings.
+            Console.Write("Loading settings... ");
+            loadSuccess = LoadSettings(out botSettings);
+            Console.WriteLine(loadSuccess ? @"Successfully loaded" : @"No file - Using default values.");
+
+            // ----------------
+            // TODO: Initiate auditing.
+
+            // ----------------
+            // TODO: Initiate SQL stuff.
+
+            // ----------------
+            // TODO: Initiate logging.
+
+            // ----------------
+            // TODO: Run the bot.
+
+
+        }
+
+        /// <summary>Load authkey and webhooks.</summary>
+        static Identity LoadIdentity()
+        {
+            Identity identity_returnVal;
+
+            if (File.Exists(Files.IdentityFile))
             {
-                Environment.Exit(0);
+                string identityFileContents;
+
+                // Read the identity file's contents
+                using (StreamReader sr = new StreamReader(Files.IdentityFile))
+                {
+                    identityFileContents = sr.ReadToEnd();
+                }
+
+                // Deserialize the object.
+                identity_returnVal = JsonConvert.DeserializeObject<Identity>(identityFileContents);
+            } 
+            else
+            {
+                throw new FileNotFoundException("Unable to find identity.json.");
             }
+
+            return identity_returnVal;
+        }
+
+        public static async Task SaveSettings()
+        {
+            throw new NotImplementedException();
+        }
+        
+        /// <summary>Load the bot's settings.</summary>
+        /// Doesn't have to be asynchronous because I don't care what performance the bot has upon start.
+        static bool LoadSettings(out BotSettings botSettings)
+        {
+            bool loadedValues_returnVal;
+
+            BotSettingsFile = new SaveFile(Files.SettingsFile);
+
+            // Check if it's an existing save file.
+            if (BotSettingsFile.IsExistingSaveFile())
+            {   // It's an existing file, so let's get the values.
+                botSettings = BotSettingsFile.Load<BotSettings>(botSettingsLock);
+                loadedValues_returnVal = true;
+            }
+            else
+            {   // It's not an existing file, so let's use default values.
+                botSettings = new BotSettings();
+                loadedValues_returnVal = false;
+            }
+
+            return loadedValues_returnVal;
         }
 
         /// <summary>Process CLI.</summary>
---
+++ FluffyEars/Stellarch/Settings/BotSettings.cs
@@ -0,0 +1,59 @@
+﻿// BotSettings.cs
+// A static class for every bot setting including their default values.
+//
+// EMIKO.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BigSister.Settings
+{
+    public class BotSettings
+    {
+        /// <summary>Default channel, FDS: #dont-ever-delete.</summary>
+        /// I have it sent to a channel in my development server.
+        const ulong DEFAULT_CHANNEL = 724187658158342204;
+
+        public BotSettings() { }
+
+        // ----------------
+        // Filter
+
+        /// <summary>Channels excluded from filtering.</summary>
+        /// Not set to any default channels. Just want to initiate it so it isn't null.
+        public List<ulong> ExcludedChannels = new List<ulong>();
+        /// <summary>The filter channel.</summary>
+        public ulong FilterChannelId = DEFAULT_CHANNEL;
+
+        // ----------------
+        // Reminder
+
+        /// <summary>Maximum reminder time allowed in months.</summary>
+        public int MaxTimeMonths = 12;
+
+        // ----------------
+        // Mention snooper
+
+        /// <summary>ID of #action-logs</summary>
+        public ulong ActionChannelId = DEFAULT_CHANNEL;
+        /// <summary>Maximum timespan for an action to be considered.</summary>
+        public int ActionTimespan = 6;
+
+        // ----------------
+        // Rimboard
+
+        /// <summary>ID of the Rimboard channel.</summary>
+        public ulong RimboardChannelId = DEFAULT_CHANNEL;
+        /// <summary>The Rimboard's pinning emote.</summary>
+        public string RimboardEmoticon = @"<:rimworld:432227067313127424>";
+        /// <summary>Number of reactions needed to pin a message in Rimboard.</summary>
+        public int ReactionsNeeded = 5;
+
+        // ----------------
+        // Role Requests
+
+        /// <summary>ID of the role request channel.</summary>
+        public ulong RoleChannel = DEFAULT_CHANNEL;
+    }
+}
