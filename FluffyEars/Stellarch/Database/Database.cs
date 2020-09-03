1951c95bba3058a7e8f5adc9e2c405449e51f812
Slate5521 <jekksie@gmail.com>
2020-09-02 21:32:55 -0700

    Moved default sql generator

---
+++ FluffyEars/Stellarch/Database/Database.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Data.Sqlite;
+
+namespace BigSister.Database
+{
+    class Database
+    {
+
+        public static void GenerateDefaultFile(string outputFile)
+        {   //Data Source=c:\mydb.db;Version=3;
+            string dataSource = $"Data Source={outputFile};";
+
+            using (var connection = new SqliteConnection(dataSource))
+            {
+                connection.Open();
+
+                using var command = connection.CreateCommand();
+
+                // --------------------------------
+                // Rimboard table.
+
+                command.CommandText =
+                    @"
+                        CREATE TABLE `Rimboard` (
+                            `OriginalMessageId` BIGINT unsigned NOT NULL, -- Snowflake of original message.
+                            `PinnedMessageId`   BIGINT unsigned NOT NULL  -- Snowflake of pinned aka reposted message.
+                        );
+                    ";
+
+                command.ExecuteNonQuery();
+
+                // --------------------------------
+                // Reminder table.
+
+                command.CommandText =
+                    @"
+                        CREATE TABLE `Reminders` (
+	                        `Id`            TINYTEXT   NOT NULL, -- Reminder ID
+	                        `Message`       MEDIUMTEXT         , -- Reminder message
+	                        `TriggerTime`   TIMESTAMP  NOT NULL, -- Reminder trigger timestamp
+	                        `Mentions`      MEDIUMTEXT NOT NULL  -- Whitespace separated mention strings
+                        );
+                    ";
+
+                command.ExecuteNonQuery();
+
+                // --------------------------------
+                // Exclude system.
+
+                command.CommandText =
+                    @"
+                        CREATE TABLE `Filter` (
+                            `Type`      TINYINT unsigned NOT NULL DEFAULT '1', -- 1 REGEX 2 EXCLUDE
+                            `String`    MEDIUMTEXT       NOT NULL              -- Mask of regex or exclude
+                        );
+                    ";
+
+                command.ExecuteNonQuery();
+
+                // --------------------------------
+                // Role request.
+
+                command.CommandText =
+                    @"
+                        CREATE TABLE `Roles` (
+	                        `MessageId` BIGINT unsigned NOT NULL, -- Snowflake of message
+	                        `RoleId`    BIGINT unsigned NOT NULL, -- Snowflake of role
+	                        `EmoteId`   BIGINT unsigned NOT NULL  -- Snowflake of emote
+                        );
+                    ";
+            }
+        }
+    }
+}
--- FluffyEars/Stellarch/Program.cs
+++ FluffyEars/Stellarch/Program.cs
@@ -88,7 +88,7 @@ namespace BigSister
             else
             { // DB not found
                 Console.WriteLine("No database - Instantiating default {0}.", localDbPath);
-                GenerateSQLFile(Files.DatabaseFile);
+                Database.Database.GenerateDefaultFile(Files.DatabaseFile);
             }
 
             // ----------------
@@ -175,71 +175,6 @@ namespace BigSister
         private static void GenerateMd5File(string inputFile, string outputFile)
         {
             throw new NotImplementedException();
-        }
-
-        private static void GenerateSQLFile(string outputFile)
-        {   //Data Source=c:\mydb.db;Version=3;
-            string dataSource = $"Data Source={outputFile};";
-
-            using (var connection = new SqliteConnection(dataSource)) 
-            {
-                connection.Open();
-
-                using var command = connection.CreateCommand();
-
-                // --------------------------------
-                // Rimboard table.
-
-                command.CommandText =
-                    @"
-                        CREATE TABLE `Rimboard` (
-                            `OriginalMessageId` BIGINT unsigned NOT NULL, -- Snowflake of original message.
-                            `PinnedMessageId`   BIGINT unsigned NOT NULL  -- Snowflake of pinned aka reposted message.
-                        );
-                    ";
-
-                command.ExecuteNonQuery();
-
-                // --------------------------------
-                // Reminder table.
-
-                command.CommandText =
-                    @"
-                        CREATE TABLE `Reminders` (
-	                        `Id`            TINYTEXT   NOT NULL, -- Reminder ID
-	                        `Message`       MEDIUMTEXT         , -- Reminder message
-	                        `TriggerTime`   TIMESTAMP  NOT NULL, -- Reminder trigger timestamp
-	                        `Mentions`      MEDIUMTEXT NOT NULL  -- Whitespace separated mention strings
-                        );
-                    ";
-
-                command.ExecuteNonQuery();
-
-                // --------------------------------
-                // Exclude system.
-
-                command.CommandText =
-                    @"
-                        CREATE TABLE `Filter` (
-                            `Type`      TINYINT unsigned NOT NULL DEFAULT '1', -- 1 REGEX 2 EXCLUDE
-                            `String`    MEDIUMTEXT       NOT NULL              -- Mask of regex or exclude
-                        );
-                    ";
-
-                command.ExecuteNonQuery();
-
-                // --------------------------------
-                // Role request.
-
-                command.CommandText =
-                    @"
-                        CREATE TABLE `Roles` (
-	                        `MessageId` BIGINT unsigned NOT NULL, -- Snowflake of message
-	                        `RoleId`    BIGINT unsigned NOT NULL, -- Snowflake of role
-	                        `EmoteId`   BIGINT unsigned NOT NULL  -- Snowflake of emote
-                        );
-                    ";
-            }
         }
     }
 }
