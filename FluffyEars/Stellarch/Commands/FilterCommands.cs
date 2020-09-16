4e41574873fcca9dcfcfcb9a1e869b4b02ac2e60
Slate5521 <jekksie@gmail.com>
2020-09-16 15:46:44 -0700

    more help documentation...

--- FluffyEars/Stellarch/Commands/FilterCommands.cs
+++ FluffyEars/Stellarch/Commands/FilterCommands.cs
@@ -21,7 +21,7 @@ namespace BigSister.Commands
         [Command("filter"), 
             MinimumRole(Role.Mod),
             Description("Depending on user input, adds a new mask, removes a mask, or lists all the masks.\n\n" +
-            "Usage:\n" +
+            **"Usage:**\n" +
             "!filter new/add <regexString> e.g. !filter new b[aeiou]nny" +
             "!filter remove/delete <regexString> e.g. !filter remove b[aeiou]nny" +
             "!filter list")]
@@ -78,7 +78,7 @@ namespace BigSister.Commands
             Aliases("excludes"), 
             MinimumRole(Role.Mod),
             Description("Depending on user input, adds a new exclude, removes an exclude, or lists all the excludes.\n\n" +
-            "Usage:\n" +
+            "**Usage:**\n" +
             "!exclude(s) new/add <excludePhrase> e.g. !exclude new bunnyes cute" +
             "!exclude(s) remove/delete <excludePhrase> e.g. !exclude remove bunnyes cute" +
             "!exclude(s) list")]
